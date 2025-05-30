5ed197e787819f889551ad60d10b4f4c6664d9d8
Asya Nikolova <asya22@abv.bg>
2025-05-30 23:54:16 +0300

    Create Cows_and_bulls.py

---
+++ Cows_and_bulls.py
@@ -0,0 +1,97 @@
+import random
+
+print("Let's play Cows and Bulls game!")
+
+#generating a random 4-digits number with no duplicate digits
+
+first_digit = str(random.randint(0, 9))
+
+second_digit = str(random.randint(0, 9))
+while second_digit == first_digit:
+    second_digit = str(random.randint(0, 9))
+
+third_digit = str(random.randint(0, 9))
+while third_digit == first_digit or third_digit == second_digit:
+    third_digit = str(random.randint(0, 9))
+
+forth_digit = str(random.randint(0, 9))
+while forth_digit == first_digit or forth_digit == second_digit or forth_digit == third_digit:
+    forth_digit = str(random.randint(0, 9))
+
+number = first_digit + second_digit + third_digit + forth_digit
+
+guess = input("Enter your first guess: ")
+attempts = 1
+
+while True:
+
+    #checking the guess is a number
+
+    try:
+        guess_number = int(guess)
+    except:
+        print("The game works with numbers only!")
+        guess = input("Please enter a 4-digit number: ")
+        continue
+
+    #making sure it has exactly 4 digits
+
+    if len(guess) != 4:
+        print("You entered an invalid number!")
+        guess = input("Please enter a 4-digit number: ")
+        continue
+
+    #making sure there are no duplicate digits in the guess
+
+    first_guessed_digit = guess[0]
+    second_guessed_digit = guess[1]
+    third_guessed_digit = guess[2]
+    forth_guessed_digit = guess[3]
+
+    error_message = ""
+
+    if first_guessed_digit in [second_guessed_digit, third_guessed_digit, forth_guessed_digit] or \
+        second_guessed_digit in [third_guessed_digit, forth_guessed_digit] or \
+        third_guessed_digit == forth_guessed_digit:
+        error_message = "The number you entered contains duplicate digits!"
+
+    if error_message:
+        print(error_message)
+        guess = input("Enter a number with 4 different digits: ")
+        continue
+
+    #calculating the number of bulls and cows
+
+    cows = 0
+    bulls = 0
+    i = 0
+
+    for digit in guess:
+        if digit in number:
+            if digit == number[i]:
+                bulls += 1
+            else:
+                cows += 1
+        i += 1
+
+    if bulls == 4:
+        if attempts == 1:
+            attempts_output = "attempt"
+        else:
+            attempts_output = "attempts"
+        print(f"Congratulations, you guessed the number {number} in {attempts} {attempts_output}!")
+        break
+
+    if bulls == 1:
+        bulls_output = "bull"
+    else:
+        bulls_output = "bulls"
+
+    if cows == 1:
+        cows_output = "cow"
+    else:
+        cows_output = "cows"
+
+    print(f"Your guess {guess} has {bulls} {bulls_output} and {cows} {cows_output}.")
+    attempts += 1
+    guess = input(f"Enter your guess {attempts}: ")
